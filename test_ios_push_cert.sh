dd89143e12ced77ba149ba67b54234edc4fa6a0e
snowcookie <snow.shanalike@gmail.com>
2019-06-11 16:15:52 +0800

    update test_ios_push_cert - add test sandbox

--- test_ios_push_cert.sh
+++ test_ios_push_cert.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-openssl s_client -connect gateway.push.apple.com:2195 -cert $1
+
+function usage() { 
+	echo 'uasge :';
+	echo $0' [-d] {cert_path}';
+	echo "  -d        : Test sandbox(develop env)";
+	echo "  cert_path : Path of cert for testing";
+	echo "";
+}
+
+endpoint='gateway.push.apple.com:2195'
+cert="$1"
+
+while getopts 'dh' flag; do
+	case "${flag}" in
+		d) endpoint='gateway.sandbox.push.apple.com:2195'; cert="$2" ;;
+		h) usage; exit 1;;
+		*) usage; exit 1;;
+	esac
+done
+
+if [ -z $cert ]; then
+	usage; exit 1;
+fi
+
+openssl s_client -connect "$endpoint" -cert "$cert"
